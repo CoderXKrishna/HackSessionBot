1925cb503ba8db958bb4bd2f7faf364c4eea00c5
CoderXKrishna <144244911+CoderXKrishna@users.noreply.github.com>
2024-03-13 23:09:56 -0700

    improve

--- HackSessionBot/modules/__init__.py
+++ HackSessionBot/modules/__init__.py
@@ -1,20 +1,16 @@
 import glob
-from os.path import basename, dirname, isfile
+from os.path import basename, dirname, isfile, splitext
 
-
-def __list_all_modules():
-    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+def list_all_modules():
+    mod_paths = glob.glob(f"{dirname(__file__)}/*.py")
 
     all_modules = [
-        basename(f)[:-3]
+        splitext(basename(f))[0]
         for f in mod_paths
-        if isfile(f)
-        and f.endswith(".py")
-        and not f.endswith("__init__.py")
+        if isfile(f) and f.endswith(".py") and f != f"{dirname(__file__)}/__init__.py"
     ]
 
-    return all_modules
+    return sorted(all_modules)
 
-
-ALL_MODULES = sorted(__list_all_modules())
-__all__ = ALL_MODULES + ["ALL_MODULES"]
+ALL_MODULES = list_all_modules()
+__all__ = ALL_MODULES + ["list_all_modules", "ALL_MODULES"]
