3d8d0e76a025f3aec4e8c0319b3b3f42e3266b2d
CoderXKrishna <144244911+CoderXKrishna@users.noreply.github.com>
2024-03-13 23:09:49 -0700

    improve

--- HackSessionBot/__main__.py
+++ HackSessionBot/__main__.py
@@ -1,19 +1,20 @@
 import asyncio
 import importlib
-
 from pyrogram import idle
-from HackSessionBot import LOG
-from HackSessionBot.modules import ALL_MODULES
-
+from HackSessionBot import LOG, ALL_MODULES
 
 async def start_bot():
     for all_module in ALL_MODULES:
-        importlib.import_module("HackSessionBot.modules." + all_module)
+        try:
+            importlib.import_module(f"HackSessionBot.modules.{all_module}")
+        except Exception as e:
+            LOG.error(f"Error importing module {all_module}: {e}")
     LOG.print("[bold yellow]✨ ʙᴏᴛ sᴛᴀʀᴛᴇᴅ")
     await idle() 
     LOG.print("[bold red]ᴄᴀɴᴄᴇʟɪɴɢ ᴀʟʟ ᴛᴀsᴋs.")
 
-
-
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(start_bot())
+    try:
+        asyncio.run(start_bot())
+    except KeyboardInterrupt:
+        LOG.print("[bold red]ᴄᴀɴᴄᴇʟʟɪɴɢ ᴀʟʟ ᴛᴀsᴋs.")
